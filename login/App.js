175f4f8c30f8afab4182d13e401a915cc1d0bdab
MalihaItsMeM <143609963+Mali0463@users.noreply.github.com>
2025-10-20 12:27:35 +0200

    login-app.js

--- login/App.js
+++ login/App.js
@@ -1,12 +1,22 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+import login from './login/screens/login';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <View style={styles.container}>
+      <login/>
+      </View> 
     </View>
+
+   
+
   );
 }
 
